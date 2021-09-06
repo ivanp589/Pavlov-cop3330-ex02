bd373557362c92d0dfd8ad6b92ebea072734c0a6
ivanp589 <ivanpav@Knights.ucf.edu>
2021-09-06 15:00:15 -0400

    finalsolu

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 /**
  * UCF COP3330 Fall 2021 Assignment 1 Solution
- *    Copyright 2021 first_name last_name
+ *    Copyright 2021 ivan pavlov
  */
 public class App 
 {
